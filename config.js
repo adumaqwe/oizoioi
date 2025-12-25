ce74274fd1d184ce950e55f759a53e09fa7bcf34
adumaqwe <132696577+adumaqwe@users.noreply.github.com>
2025-12-25 14:36:11 +0700

    Update CONFIG messages for Christmas greeting

--- config.js
+++ config.js
@@ -1,16 +1,15 @@
 const CONFIG = {
-    introTitle: 'Giáng sinh rồi, bé có biết không!',
-    introDesc: `Như! bé đọc được dòng này,bé phải mua thuốc đau lưng vì anh làm món quà nhỏ này cho bé đó,anh dởn đấy, happy nha bé:3 `,
-    btnIntro: 'Bé bấm vào đi!',
-    title: 'Anh chân thành muốn trao bé một xíu happy ngọt ngào này 🥳',
-    desc: 'Anh thấy được chiếc lùn, đóa là bé:3 ',
-    btnYes: 'Vâng <33',
-    btnNo: 'Bé không biết :3',
-    question:' Bé muốn gửi gì cho anh hông!',
+    introTitle: 'Giáng sinh rồi đó em yêu!',
+    introDesc: `Phụng à! Luôn lạc quan và yêu đời nha em 😘 `,
+    btnIntro: 'Em bấm vào đi!',
+    title: 'Anh chân thành muốn trao cho em một xíu happy ngọt ngào này 🥳',
+    btnYes: 'Vâng <3',
+    btnNo: 'Em không biết :3',
+    question:'Em có muốn gửi gì cho anh hông!',
     btnReply: 'Gửi luôn anh <3',
     reply: 'Anh dễ thương và cute hihi <33333333',
-    mess: 'Anh biết bé nói vậy mà, anh chân thành cảm ơn bé:3',
-    messDesc: 'Bấm vào bên dưới đi bé, đừng hồi hộp, anh cũng hồi hộp nữa hihi',
-    btnAccept: 'Bấm nè tin tin <3 ',
-    messLink: 'sinhnhatem-ne-master/happy-birthday-master/index.html' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    mess: 'Anh biết em nói vậy mà, anh chân thành cảm ơn em :3',
+    messDesc: 'Bấm vào bên dưới đi em, đừng hồi hộp ~~',
+    btnAccept: '항상 사랑해 <3',
+    messLink: 'https://m.me/azzuuree303' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
